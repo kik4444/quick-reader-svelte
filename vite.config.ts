d28250b1f1ec1cd14e273db55d00c5a713dd36d9
kik4444 <kik4444@email>
2023-04-08 00:15:01 +0300

    Added custom import aliases for Vite and TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -13,6 +14,13 @@ export default defineConfig(async () => ({
       ],
     }),
   ],
+  resolve: {
+    alias: {
+      "$": path.resolve(__dirname, "./src"),
+      "$lib": path.resolve(__dirname, "./src/lib"),
+      "$stores": path.resolve(__dirname, "./src/lib/stores"),
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
