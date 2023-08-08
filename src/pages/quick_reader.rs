acfb887db11107bbb477669062b4dc05900369af
kik4444 <7779637+kik4444@users.noreply.github.com>
2023-08-08 13:14:01 +0300

    Basic reader logic

--- src/pages/quick_reader.rs
+++ src/pages/quick_reader.rs
@@ -1,35 +1,206 @@
-use leptos::{
-    html::{Textarea, P},
-    *,
-};
+/*
+ *    This file is part of Quick Reader.
+ *
+ *    Quick Reader is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Quick Reader is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Quick Reader.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-use crate::splitter::TextChunk;
+use std::time::Duration;
+
+use leptos::{html::Textarea, leptos_dom::helpers::IntervalHandle, *};
+use wasm_bindgen::JsValue;
+
+use crate::splitter;
 
 #[derive(Debug)]
-pub struct AppData {
+pub struct ReaderState {
     pub text: String,
-    pub current_chunk: usize,
-    pub chunks: Vec<TextChunk>,
-    pub wpm: usize,
     pub chunk_size: usize,
+    pub current_index: usize,
+    pub words_per_minute: u64,
 }
 
 #[component]
 pub fn QuickReader() -> impl IntoView {
-    let area = create_node_ref::<Textarea>();
-    let display = create_node_ref::<P>();
+    let reader_state = create_rw_signal(ReaderState {
+        text: r#"Welcome to "Quick Reader". Press start to begin reading quickly."#.into(),
+        chunk_size: 1,
+        current_index: 0,
+        words_per_minute: 300,
+    });
 
-    set_timeout(
-        move || {
-            // let chunks = splitter::split(&area().unwrap().value(), 0).unwrap();
-            display().unwrap().set_inner_text("Changed");
+    let playing = create_rw_signal(false);
+
+    let (text, set_text) = create_slice(
+        reader_state,
+        |s| s.text.clone(),
+        move |s, new| {
+            s.text = new;
+            s.current_index = 0;
+            playing.set(false);
         },
-        std::time::Duration::from_secs(1),
     );
 
+    let (chunk_size, set_chunk_size) = create_slice(
+        reader_state,
+        |s| s.chunk_size,
+        |s, new| {
+            s.chunk_size = new;
+            // TODO recalculate new index
+        },
+    );
+
+    let (current_index, set_current_index) = create_slice(
+        reader_state,
+        |s| s.current_index,
+        |s, new| s.current_index = new,
+    );
+
+    let (words_per_minute, set_words_per_minute) = create_slice(
+        reader_state,
+        |s| s.words_per_minute,
+        |s, new| {
+            if new > 1000 {
+                s.words_per_minute = 1000
+            } else if new < 100 {
+                s.words_per_minute = 100
+            } else {
+                s.words_per_minute = new
+            }
+        },
+    );
+
+    let text_chunks = create_memo(move |_| splitter::split(&text(), chunk_size()));
+
+    let textarea = create_node_ref::<Textarea>();
+    let textarea_locked = create_rw_signal(false);
+
+    let reset = move || {
+        set_words_per_minute(300);
+        set_chunk_size(1);
+    };
+
+    let stop = move || {
+        playing.set(false);
+        textarea_locked.set(false);
+        set_current_index(0);
+
+        if let Ok(Some(selection)) = window().get_selection() {
+            _ = selection.empty();
+        }
+
+        // We must unfocus the textfield after stopping play to avoid the user accidentally
+        // deleting text if they press space after playing has finished
+        _ = textarea().expect("to exist").blur();
+    };
+
+    let restart = move || {
+        set_current_index(0);
+        _ = textarea().expect("to exist").focus();
+    };
+
+    let toggle_playing = move || {
+        if playing() {
+            playing.set(false);
+        } else {
+            playing.set(true);
+            textarea_locked.set(true);
+
+            // On Windows we must focus the textfield otherwise the highlight is not visible
+            _ = textarea().expect("to exist").focus();
+        }
+    };
+
+    let step = move || {
+        if playing() {
+            if current_index() < text_chunks.with(|t| t.len() - 1) {
+                set_current_index(current_index() + 1);
+            } else {
+                stop()
+            }
+
+            let (start, stop) = text_chunks.with(|t| {
+                let curr = current_index();
+                (t[curr].start_offset, t[curr].stop_offset)
+            });
+
+            _ = textarea()
+                .expect("to exist")
+                .set_selection_range(start as u32, stop as u32);
+        }
+    };
+
+    create_effect(move |prev: Option<Result<IntervalHandle, JsValue>>| {
+        if let Some(Ok(handle)) = prev {
+            handle.clear();
+        }
+
+        set_interval_with_handle(step, Duration::from_millis(words_per_minute()))
+    });
+
+    window_event_listener(ev::keydown, move |ev| match ev.code().as_str() {
+        "ArrowLeft" => set_current_index(current_index().saturating_sub(5)),
+        "ArrowRight" => set_current_index(std::cmp::max(
+            current_index() + 5,
+            text_chunks.with(|t| t.len() - 1),
+        )),
+        "Space" => {
+            if textarea_locked()
+                || document().active_element().expect("to exist") != ***textarea().expect("")
+            {
+                ev.prevent_default();
+                toggle_playing();
+            }
+        }
+        _ => (),
+    });
+
     view! {
-      <textarea cols="29" rows="10" node_ref=area></textarea>
-      <button>"click me"</button>
-      <p node_ref=display>"To show"</p>
+      <textarea
+        cols="30"
+        rows="10"
+        readonly=textarea_locked
+        node_ref=textarea
+        on:input=move |ev| set_text(event_target_value(&ev))
+      >
+        {text}
+      </textarea>
+      <p>{move || text_chunks.with(|t| t[current_index()].chunk.clone())}</p>
+      <button on:click=move |_| stop() disabled=move || !textarea_locked()>
+        "Stop"
+      </button>
+      <br/>
+      <button on:click=move |_| restart() disabled=move || !textarea_locked()>
+        "Restart"
+      </button>
+      <br/>
+      <button on:click=move |_| toggle_playing()>
+        {move || if playing() { "Pause" } else { "Play" }}
+      </button>
+      <br/>
+      <input
+        type="number"
+        value=chunk_size
+        on:input=move |ev| set_chunk_size(event_target_value(&ev).parse().unwrap_or(1))
+      />
+      <br/>
+      <input
+        type="number"
+        step="50"
+        min="100"
+        max="1000"
+        value=words_per_minute
+        on:input=move |ev| set_words_per_minute(event_target_value(&ev).parse().unwrap_or(300))
+      />
     }
 }
