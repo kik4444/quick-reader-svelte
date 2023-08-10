dff0adee1d12731ee385b5ff8309a8044569a64c
kik4444 <7779637+kik4444@users.noreply.github.com>
2023-08-10 19:58:56 +0300

    Add reset button

--- src/pages/quick_reader.rs
+++ src/pages/quick_reader.rs
@@ -239,6 +239,13 @@ pub fn QuickReader() -> impl IntoView {
                     }
                 })}
 
+            <button
+              class="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              on:click=move |_| reset()
+            >
+              "Reset"
+            </button>
+
           </div>
 
           <div id="playback" class="w-full grid grid-cols-3 px-5 gap-5">
