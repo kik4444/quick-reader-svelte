ca5580be35e5a86858d0cd840ef43a4d2567b4c7
kik4444 <7779637+kik4444@users.noreply.github.com>
2023-08-17 01:29:43 +0300

    Add saving and loading settings in Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,6 +18,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::Write;
+
+use common::AppSettings;
+use tauri::AppHandle;
+
+#[tauri::command]
+async fn load_settings(app_handle: AppHandle) -> Result<AppSettings, String> {
+    let settings_dir = app_handle
+        .path_resolver()
+        .app_config_dir()
+        .ok_or_else(|| "could not resolve config dir".to_string())?;
+
+    let reader = std::fs::OpenOptions::new()
+        .read(true)
+        .open(settings_dir.join("settings.json"))
+        .map_err(|e| format!("cannot open settings.json: {e}"))?;
+
+    serde_json::from_reader(reader).map_err(|e| format!("error reading settings.json: {e}"))
+}
+
+#[tauri::command]
+async fn save_settings(app_handle: AppHandle, settings_serialized: &str) -> Result<(), String> {
+    let settings_dir = app_handle
+        .path_resolver()
+        .app_config_dir()
+        .ok_or_else(|| "could not resolve config dir".to_string())?;
+
+    if !settings_dir.is_dir() {
+        std::fs::create_dir(&settings_dir).map_err(|e| format!("error creating dir: {e}"))?;
+    }
+
+    let mut file = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(settings_dir.join("settings.json"))
+        .map_err(|e| format!("error writing settings.json: {e}"))?;
+
+    file.write_all(settings_serialized.as_bytes())
+        .map_err(|e| format!("error saving settings.json: {e}"))
+}
+
 #[tauri::command]
 async fn get_system_fonts() -> Result<Vec<String>, String> {
     font_kit::sources::fs::FsSource::new()
@@ -27,7 +69,11 @@ async fn get_system_fonts() -> Result<Vec<String>, String> {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_system_fonts])
+        .invoke_handler(tauri::generate_handler![
+            load_settings,
+            save_settings,
+            get_system_fonts
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
